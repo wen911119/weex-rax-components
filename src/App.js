7560a391da6f36d32e9edfe0213b5f3029306cab
wen911119 <wen911119@gmail.com>
2018-06-08 13:59:36 +0800

    完善layoutview

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { createElement, Component } from 'rax'
 import View from 'rax-view'
 import Text from './components/Text'
-import { RowView, ColumnView } from './components/LayoutView'
+import {
+  RowView,
+  ColumnView,
+  XCenterView,
+  SlotRowView,
+  SlotColumnView
+} from './components/LayoutView'
 import styles from './App.css'
 
 class App extends Component {
@@ -13,6 +19,22 @@ class App extends Component {
           edit src/App.js and save to reload.To get started, edit src/App.js and
           save to reload.To get started, edit src/App.js and save to reload.
         </Text>
+        <XCenterView height={200}>
+          <Text>绝对居中</Text>
+        </XCenterView>
+        <SlotRowView gap={20}>
+          <Text>11111</Text>
+          <Text>22222</Text>
+          <Text>33333</Text>
+          <Text>44444</Text>
+          <Text>55555</Text>
+          <Text>66666</Text>
+        </SlotRowView>
+        <SlotColumnView gap={20}>
+          <Text>11111</Text>
+          <Text>22222</Text>
+          <Text>33333</Text>
+        </SlotColumnView>
         <RowView height={300} bgColor='#f8584f' padding={[0, 30, 0, 30]}>
           <Text lines={2} size={30} width={300}>
             To get started, edit src/App.js and save to reload.To get started,
--- src/components/LayoutView/index.js
+++ src/components/LayoutView/index.js
@@ -1,5 +1,11 @@
-import { createElement } from 'rax'
+import { createElement, cloneElement } from 'rax'
 import View from 'rax-view'
+
+function alternateInsert (arr, item) {
+  let insertedArr = arr.reduce((all, current) => all.concat(current, item), [])
+  insertedArr.pop()
+  return insertedArr
+}
 
 export const RowView = ({
   children,
@@ -72,3 +78,59 @@ export const ColumnView = ({
     {children}
   </View>
 
+
+export const XCenterView = ({
+  children,
+  style = {},
+  height,
+  width,
+  bgColor = 'transparent'
+}) =>
+  <View
+    style={Object.assign(
+      {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: bgColor,
+        height: height,
+        width: width
+      },
+      style
+    )}
+  >
+    {children}
+  </View>
+
+
+export const Slot = ({ gap }) => <View style={{ width: gap }} />
+
+export const SlotRowView = ({ gap = 0, children, ...otherProps }) => {
+  const allChildren = alternateInsert(children, <Slot />)
+  return (
+    <RowView {...otherProps}>
+      {allChildren.map(child => {
+        if (child.type.name === 'Slot') {
+          return cloneElement(child, { gap })
+        }
+        return child
+      })}
+    </RowView>
+  )
+}
+
+export const Gap = ({ gap }) => <View style={{ height: gap }} />
+
+export const SlotColumnView = ({ gap = 0, children, ...otherProps }) => {
+  const allChildren = alternateInsert(children, <Gap />)
+  return (
+    <ColumnView {...otherProps}>
+      {allChildren.map(child => {
+        if (child.type.name === 'Gap') {
+          return cloneElement(child, { gap })
+        }
+        return child
+      })}
+    </ColumnView>
+  )
+}
